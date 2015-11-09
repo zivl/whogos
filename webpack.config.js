01dc15d17eb5a672e2345f10ad0f0448d1a36dc8
Ziv Levy <levi.ziv@gmail.com>
2015-11-10 00:27:55 +0200

    adding gulp + webpack for dev phase and production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,47 @@
 function getEntrySources(sources) {
-  if (process.env.NODE_ENV !== 'production') {
-    sources.push('webpack-dev-server/client?http://localhost:8080');
-    sources.push('webpack/hot/only-dev-server');
-  }
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+        sources.push('webpack/hot/only-dev-server');
+    }
 
-  return sources;
+    return sources;
 }
 
 module.exports = {
-  devtool: process.env.NODE_ENV !== 'production' ? 'eval-source-map' : '',
-  entry: {
-    bundle: getEntrySources([
-      './src/app.js'
-    ])
-  },
-  output: {
-    publicPath: 'http://localhost:8080/',
-    filename: 'dist/[name].js'
-  },
-  devServer: {
-    port: 8080,
-    historyApiFallback: true
-  },
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      loader: 'source-map-loader'
-    }],
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel-loader?stage=0'],
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      loaders: ['style', 'css', 'sass']
-    }]
-  }
+    devtool: process.env.NODE_ENV !== 'production' ? 'eval-source-map' : '',
+    entry: {
+        bundle: getEntrySources([
+            './src/app.js'
+        ])
+    },
+    output: {
+        path: "/",
+        // no real path is required, just pass "/"
+        // but it will work with other paths too.
+        publicPath: 'http://localhost:8080/',
+        filename: 'dist/[name].js'
+    },
+    devServer: {
+        port: 8080,
+        historyApiFallback: true
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            }],
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel-loader?stage=0'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        ]
+    }
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,20 +5,17 @@ var config = {
     entry: path.resolve(__dirname, 'src/app.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
+        publicPath: 'http://localhost:8080/', // This is used to generate URLs to e.g. images
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
+        loaders: [
+            { test: /\.js$/,  exclude: [node_modules_dir], loader: 'babel?stage=0' },
+            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
 
-            // There is not need to run the loader through
-            // vendors
-            exclude: [node_modules_dir],
-            loader: 'babel?stage=0'
-        }, {
-            test: /\.scss$/,
-            loaders: ['style', 'css', 'sass']
-        }]
+            // inline base64 URLs for <=8k images, direct URLs for the rest]
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+        ]
     }
 };
 
